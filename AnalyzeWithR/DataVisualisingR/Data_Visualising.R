5b67386048b58292f5d03e1646d6a7da3bca8055
Tarık Kırlıoğlu <tarik.kirliogllu@gmail.com>
2025-01-29 00:44:06 +0300

    Add files via upload

---
+++ AnalyzeWithR/DataVisualisingR/Data_Visualising.R
@@ -0,0 +1,78 @@
+#importing libraries
+library(ggplot2)
+library(tidyverse)
+library(pheatmap)
+library(viridis)
+
+#seeing datasets
+data()
+
+
+diamonds <- diamonds %>% ggplot(aes(fct_infreq(cut))) +
+  geom_bar(fill = "blue", alpha = 0.5) +
+  labs(x = "Diamond Cut", y = "Count", title = "Distribution of Diamond Cuts") +
+  theme_bw()
+
+
+DNase <- DNase %>% 
+  ggplot(aes(conc, density, colour = Run)) +
+  geom_line(size=1) +
+  geom_point(alpha=0.5, size=5) +
+  labs(title = "Elisa assay of DNase",
+       x = "Concentration",
+       y ="Density") +
+  theme_bw()
+
+PlantGrowth <- PlantGrowth %>% 
+  ggplot(aes(group, weight, fill = group)) +
+  geom_boxplot(alpha=0.5) +
+  labs(x="Group",
+       y="Weight",
+       title="Results from an Experiment on Plant Growth") +
+  theme_bw()
+
+
+airquality <- airquality %>% 
+  drop_na() %>% 
+  mutate(Month = gsub( 5 ,"May",Month),
+         Month = gsub( 6 ,"June",Month),
+         Month = gsub( 7 ,"July",Month),
+         Month = gsub( 8 ,"August",Month),
+         Month = gsub( 9 ,"September",Month)) %>% 
+         ggplot(aes(Ozone, Solar.R) ) +
+         geom_point(alpha= 0.7, aes(size=Wind, color=Temp)) +
+         geom_smooth(method = lm, se=F) +
+         facet_wrap(~Month) +
+         labs(x = "Ozone",
+              y ="Solar radiation",
+              title = "New York Air Quality Measurements") +
+         theme_bw()
+     
+
+correlation <- cor(mtcars)
+
+corrr <- pheatmap(correlation, 
+         color = viridis(100), 
+         display_numbers = TRUE,
+         main = "Correlation Matrix") 
+
+png("airquality.png", width = 2400, height = 1600, res = 360)
+airquality
+dev.off()
+
+png("corrr.png", width = 2400, height = 1600, res = 360)
+corrr
+dev.off()
+
+png("DNase.png", width = 2400, height = 1600, res = 360)
+DNase
+dev.off()
+
+png("PlantGrowth.png", width = 2400, height = 1600, res = 360)
+PlantGrowth
+dev.off()
+
+
+png("diamonds.png", width = 2400, height = 1600, res = 360)
+diamonds
+dev.off()
